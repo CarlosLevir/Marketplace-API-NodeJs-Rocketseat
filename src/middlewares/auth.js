ea55c0dc7e1413a0e98e40bebf5ac8be252d2414
Carlos Levir <carloslevirfs@gmail.com>
2019-05-15 17:11:15 -0300

    Build of Auth Middleware.

---
+++ src/middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+const authConfig = require('../config/auth');
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) return res.status(401).json({ error: 'No token provided' });
+
+  const parts = authHeader.split(' ');
+
+  if (!parts.lenght !== 2) return res.status(401).json({ error: 'Token format error' });
+
+  const [, token] = parts;
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
