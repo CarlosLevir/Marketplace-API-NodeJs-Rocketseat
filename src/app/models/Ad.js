cf9bacb2c2f1701f3197d54e1f3d6f388dca4ad1
Carlos Levir <carloslevirfs@gmail.com>
2019-05-15 17:10:38 -0300

    Build of Ad Model.

---
+++ src/app/models/Ad.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Ad = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('Ad', Ad);
