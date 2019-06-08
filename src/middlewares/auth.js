a91bd287773de775c77a3d3c17b79312ed7ffab7
Carlos Levir <carloslevirfs@gmail.com>
2019-06-08 10:06:58 -0300

    Auth Middleware Upgrade.

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,24 +1,28 @@
 const jwt = require('jsonwebtoken');
-const { promisify } = require('util');
-const authConfig = require('../config/auth');
 
-module.exports = async (req, res, next) => {
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader) return res.status(401).json({ error: 'No token provided' });
+  if (!authHeader) return res.status(401).json({ error: 'Nenhum token foi fornecido' });
 
   const parts = authHeader.split(' ');
 
-  if (parts.length !== 2) return res.status(401).json({ error: 'Token format error' });
+  if (parts.length !== 2) return res.status(401).json({ error: 'Formato do token incorreto' });
 
-  const [, token] = parts;
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) return res.status(401).json({ error: 'Token mal formatado' });
 
   try {
-    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-    req.userId = decoded.id;
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) return res.status(401).json({ error: 'Token inválido' });
 
-    return next();
+      req.userId = decoded.id;
+
+      return next();
+    });
   } catch (err) {
-    return res.status(401).json({ error: 'Invalid token' });
+    return res.status(500).json({ error: 'Ocorreu um erro' });
   }
 };
